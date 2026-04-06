2191581d610e7d0065541a05b3331f916438647b
limamichelly-commits <lima.michelly@escolar.ifrn.edu.br>
2026-04-06 11:57:23 -0300

    06/04

--- 1bm.project/Assets/Script/AudioManager.cs
+++ 1bm.project/Assets/Script/AudioManager.cs
@@ -1,7 +1,14 @@
-using UnityEngine;
+using System.Collections.Generic;using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    #region Private Fields
+       
+       private List<AudioSource> systemSourceChannels;
+       private List<AudioSource> ActiveSources;
+       
+       #endregion
+       
     #region Singleton 
     public static AudioManager instance;
     
@@ -11,6 +18,8 @@ public class AudioManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+            systemSourceChannels = new List<AudioSource>();
+            ActiveSources = new List<AudioSource>();
         }
         else
         {
@@ -19,5 +28,45 @@ public class AudioManager : MonoBehaviour
     }
 
     #endregion
+    
+    #region Play 2D Sonds
+
+    public void playMusic(AudioClip clip)
+    {
+        if (systemSourceChannels.Count == 0)
+        {
+            systemSourceChannels.Add(gameObject.AddComponent<AudioSource>());
+        }
+        systemSourceChannels[0].Stop();
+        systemSourceChannels[0].clip = clip;
+        systemSourceChannels[0].Play();
+    }
+
+    public void StopMusic()
+    {
+        if (systemSourceChannels.Count > 0)
+        {
+            systemSourceChannels[0].Stop();
+        }
+            
+    }
+
+    public void pauseMusic()
+    {
+        if (systemSourceChannels.Count > 0)
+        {
+            systemSourceChannels[0].UnPause();
+        }
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        if (systemSourceChannels.Count == 0)
+        {
+            systemSourceChannels.Add(gameObject.AddComponent<AudioSource>());
+        }
+        systemSourceChannels[0].PlayOneShot(clip);
+    }
+    #endregion
 }
-  
+   
