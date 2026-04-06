0dbf77838a2c4fa79ece65cc76dc3707677c3d81
limamichelly-commits <lima.michelly@escolar.ifrn.edu.br>
2026-04-06 11:12:05 -0300

    06/04

---
+++ 1bm.project/Assets/Script/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Singleton 
+    public static AudioManager instance;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
+  
